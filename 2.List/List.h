3d08ff7b420404e1ea8f16305387cf99694d434f
radium99 <115422121+radium99@users.noreply.github.com>
2026-02-13 10:14:19 +0900

    List / ListIterator 진행
    - List 기존 코드에서 문제 있는 부분 수정
    - ListIterator 만들고, 개념 설명
    - 포인터를 배열로 확인할 때 유용한 debug 구문 설명 (예: list.data, 10)

--- 2.List/List.h
+++ 2.List/List.h
@@ -2,11 +2,105 @@
 
 #include <iostream>
 #include <cassert>
+template<typename List>
+class ListInterator
+{
+public:
+	// typename List::
+	// List가 템플릿 파라미터로 넘어오는 경우 typename 까지 지정.
+	using ValueType = typename List::ValueType;
+	using PointerType = ValueType*;
+	using ReferenceType = ValueType&;
 
+public:
+	ListInterator(PointerType ptr)
+		: ptr(ptr)
+	{
+
+	}
+
+	// 전위 증가 연산자. 참고로 this는 포인터라 레퍼런스로 반환이 되지 않는다.
+	ListInterator& operator++()
+	{
+		++ptr;
+		return *this;
+	}
+
+	// 후위 증가 연산자.
+	ListInterator& operator++(int)
+	{
+		// 현재 반복자를 임시 저장.
+		ListInterator iterator = *this;
+
+		// 내부 포인터 ++ 처리.
+		++(*this);
+
+		// 앞서 저장했던 반복자 반환.
+		return iterator;
+	}
+
+	// 전위 감소 연산자.
+	ListInterator& operator--()
+	{
+		--ptr;
+		return *this;
+	}
+
+	// 후위 감소 연산자.
+	ListInterator& operator--(int)
+	{
+		// 현재 반복자를 임시 저장.
+		ListInterator iterator = *this;
+
+		// 내부 포인터 -- 처리.
+		--(*this);
+
+		// 앞서 저장했던 반복자 반환.
+		return iterator;
+	}
+
+	ReferenceType operator[](int index)
+	{
+		//알아서 자료형 만큼 점프해줌.
+		return *(ptr + index);
+	}
+
+	PointerType operator->()
+	{
+
+		return ptr;
+
+	}
+
+	PointerType operator*()
+	{
+		return *ptr;
+	}
+
+	// 비교 연산자 오버로딩.
+	bool operator==(const ListInterator& other) const
+	{
+		return ptr == other.ptr;
+	}
+
+	bool operator!=(const ListInterator& other) const
+	{
+		return !(*this== other);
+	}
+	
+private:
+	// PosintType은 알리아싱한 것이기 때문에 ValueType*이다.
+	// 반복자는 결국 포인터.
+	PointerType ptr = nullptr;
+};
 // 자동으로 크기가 늘어나는 배열 (List/Vector).
 template<typename T>
 class List
 {
+public: 
+	using ValueType = T;
+	using Interator = ListInterator<List<T>>;
+
 public:
 	List()
 	{
@@ -70,6 +164,20 @@ public:
 	int Size() const { return size; }
 	int Capacity() const { return capacity; }
 
+	// 범위 기반 루프 처리를 위한 함수 작성 (begin/end).
+	// 배열의 첫 위치를 반환하는 함수.
+	Iterator begin()
+	{
+		//return Iterator(data);
+		return data;
+	}
+
+	// 배열에 저장된 마지막 요소의 다음 위치를 반환하는 함수.
+	Iterator end()
+	{
+		return Iterator(data + size);
+	}
+
 private:
 	// 저장 공간 할당(재할당)하는 함수.
 	void Reallocate(int newCapacity)
@@ -88,8 +196,12 @@ private:
 		//{
 		//	newBlock[ix] = data[ix];
 		//}
+		
 		// 메모리 복사.
-		memcpy(newBlock, data, sizeof(T) * capacity);
+		if (data)
+		{
+			memcpy(newBlock, data, sizeof(T) * size);
+		}
 
 		// 3. 기존 배열 공간 해제.
 		delete[] data;
--- 2.List/Main.cpp
+++ 2.List/Main.cpp
@@ -1,9 +1,31 @@
 #include "List.h"
 #include <iostream>
+#include <vector>
 
 int main()
 {
 	List<int> list;
+	for (int ix = 0; ix < 10; ++ix)
+	{
+		list.Add(ix + 1);
+	}
 
-	std::sin.get();
+	// Range-based loop.
+	std::cout << "Range-based loop\n";
+	for (const int item : list)
+	{
+		std::cout << item << "\n";
+	}
+
+	// loop - Iterator.
+	/*std::cout << "Iterator based loop\n";
+	for (
+		List<int>::Iterator it = list.begin();
+		it != list.end();
+		++it)
+	{
+		std::cout << *it << "\n";
+	}*/
+
+	std::cin.get();
 }
